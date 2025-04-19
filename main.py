1ccde949f7c92902908999dab37b70d8c8b85015
zerosand1 <priyansh.23raj@gmail.com>
2025-04-20 00:21:49 +0530

    add sys command

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from stats import get_num_words, get_character_count
+from stats import get_num_words, get_character_count, sort_char_list
+import sys
 
+if len(sys.argv) <2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+    
+path = sys.argv[1]
+    
 
 
 def get_book_text(file_path):
@@ -11,9 +18,19 @@ def get_book_text(file_path):
 
 
 def main():
-    sentence  = get_book_text("books/frankenstein.txt")
+    sentence  = get_book_text(path)
     count = get_num_words(sentence)
     char = get_character_count(sentence)
-    print(f"{count} words found in the document")
-    print(char)
+    sort_list = sort_char_list(char)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+    for item in sort_list:
+        for k, v in item.items():
+            if k.isalpha():
+                print(f"{k}: {v}")
+    print("============= END ===============")
+    
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+from operator import itemgetter
+
+
 def get_num_words(string):
     string_list = string.split()
     count = len(string_list)
@@ -12,4 +15,16 @@ def get_character_count(text):
         else:
             char_dict[char] = 1
     return char_dict
+
+
+
+
+def sort_char_list(char_dict):
+    char_list = []
+    for k,v in char_dict.items():
+        char_list.append({k: v})
+    def value_of_dict(d):
+        return list(d.values())[0]
+    char_list.sort(key=value_of_dict, reverse=True)
+    return char_list
 
